1b81db117bad08f1a60636db66c611594e744ff3
TA1059 <muchsen.ft21@mail.umy.ac.id>
2022-10-17 17:05:30 +0700

    Add project files.

---
+++ quickshort/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace QuickShort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int[] cmp_count = 0;
+        private int[] mov_count = 0;
+
+        //number of element in array
+        private int n;
+
+        void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n < 20)
+                    break;
+                else
+                    Console.WriteLine("\n Array can have maximum 20 elements \n");
+
+            }
+            Console.WriteLine("\n =================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n==================");
+
+            //get array elements
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+
+        }
+
+    }
+}
